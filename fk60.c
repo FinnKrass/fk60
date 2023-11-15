f4c871abfd59af9a639a8b85df88dd30ccc65259
Alin Marin Elena <alin@elena.space>
2023-11-15 06:30:44 +0000

    Update fk60.c

--- fk60.c
+++ fk60.c
@@ -15,17 +15,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "fsst60.h"
-
-bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-    if(res) {
-        // writePin sets the pin high for 1 and low for 0.
-        // In this example the pins are inverted, setting
-        // it low/0 turns it on, and high/1 turns the LED off.
-        // This behavior depends on whether the LED is between the pin
-        // and VCC or the pin and GND. For this, the LED is tied to GND.
-        writePin(GP17, !led_state.caps_lock);
-    }
-    return res;
-}
+#include "fk60.h"
